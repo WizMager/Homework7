61121f362610df34647e380e43c3b52b57c079ec
Anton <wizmager@gmail.com>
2021-02-03 01:50:45 +0300

    Add project files.

---
+++ Task01/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Task01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.стартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playMenuStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelRandomNumber = new System.Windows.Forms.Label();
+            this.lblRndNumTxt = new System.Windows.Forms.Label();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCommand1.Location = new System.Drawing.Point(147, 26);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand1.TabIndex = 0;
+            this.btnCommand1.Text = "button1";
+            this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.Click += new System.EventHandler(this.btnCommand1_Click);
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCommand2.Location = new System.Drawing.Point(147, 55);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(75, 23);
+            this.btnCommand2.TabIndex = 1;
+            this.btnCommand2.Text = "button2";
+            this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.Click += new System.EventHandler(this.btnCommand2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReset.Location = new System.Drawing.Point(147, 84);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "button3";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(30, 55);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(35, 13);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "label1";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.стартToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(234, 24);
+            this.menuStrip.TabIndex = 4;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // стартToolStripMenuItem
+            // 
+            this.стартToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playMenuStrip});
+            this.стартToolStripMenuItem.Name = "стартToolStripMenuItem";
+            this.стартToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.стартToolStripMenuItem.Text = "Меню";
+            // 
+            // playMenuStrip
+            // 
+            this.playMenuStrip.Name = "playMenuStrip";
+            this.playMenuStrip.Size = new System.Drawing.Size(112, 22);
+            this.playMenuStrip.Text = "Играть";
+            this.playMenuStrip.Click += new System.EventHandler(this.playMenuStrip_Click);
+            // 
+            // labelRandomNumber
+            // 
+            this.labelRandomNumber.AutoSize = true;
+            this.labelRandomNumber.Location = new System.Drawing.Point(30, 115);
+            this.labelRandomNumber.Name = "labelRandomNumber";
+            this.labelRandomNumber.Size = new System.Drawing.Size(0, 13);
+            this.labelRandomNumber.TabIndex = 5;
+            // 
+            // lblRndNumTxt
+            // 
+            this.lblRndNumTxt.AutoSize = true;
+            this.lblRndNumTxt.Location = new System.Drawing.Point(30, 94);
+            this.lblRndNumTxt.Name = "lblRndNumTxt";
+            this.lblRndNumTxt.Size = new System.Drawing.Size(91, 13);
+            this.lblRndNumTxt.TabIndex = 6;
+            this.lblRndNumTxt.Text = "Нужно получить:";
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(147, 113);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(75, 23);
+            this.btnUndo.TabIndex = 7;
+            this.btnUndo.Text = "Ход назад";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 161);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.lblRndNumTxt);
+            this.Controls.Add(this.labelRandomNumber);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCommand2);
+            this.Controls.Add(this.btnCommand1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximumSize = new System.Drawing.Size(250, 200);
+            this.MinimumSize = new System.Drawing.Size(200, 175);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem стартToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playMenuStrip;
+        private System.Windows.Forms.Label labelRandomNumber;
+        private System.Windows.Forms.Label lblRndNumTxt;
+        private System.Windows.Forms.Button btnUndo;
+    }
+}
+
---
+++ Task01/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task01
+{
+    //    1. а) Добавить в программу «Удвоитель» подсчет количества отданных команд.
+    //       б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число должен получить игрок.
+    //          Игрок должен постараться получить это число за минимальное количество ходов.
+    //       в) * Добавить кнопку «Отменить», которая отменяет последние ходы.
+    public partial class Form1 : Form
+    {
+        private int counter = 0;
+        private Dictionary<int, int> undoNumbers = new Dictionary<int, int>();
+        private bool isStart = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnCommand1.Text = "+1";
+            btnCommand2.Text = "x2";
+            btnReset.Text = "Сброс";
+            lblNumber.Text = "0";
+            Text = "Удвоитель";
+            lblRndNumTxt.Visible = false;
+            btnUndo.Visible = false;
+        }
+
+        private void btnCommand1_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            counter++;
+            UndoNumberPlus();
+            CheckMatch();
+        }
+
+        private void btnCommand2_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+            counter++;
+            UndoNumberPlus();
+            CheckMatch();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = "0";
+            counter++;
+        }
+
+        #region Task 1.1(б)
+        private void playMenuStrip_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int number = random.Next(1, 100); ;
+            MessageBox.Show($"Вам унжно получить число {number}.");
+            labelRandomNumber.Text = number.ToString();
+            lblNumber.Text = "0";
+            lblRndNumTxt.Visible = true;
+            btnUndo.Visible = true;
+            undoNumbers[0] = 0;
+            isStart = true;
+        }
+
+        private void CheckMatch()
+        {
+            if (isStart)
+            {
+                if (int.Parse(lblNumber.Text) == int.Parse(labelRandomNumber.Text))
+                {
+                    MessageBox.Show($"Вы подобрали нужное число за {counter} ходов.");
+                    lblNumber.Text = "0";
+                    labelRandomNumber.Text = " ";
+                    lblRndNumTxt.Visible = false;
+                    counter = 0;
+                    btnUndo.Visible = false;
+                    isStart = false;
+                }
+            }
+        }
+        #endregion
+
+        #region Task 1.3(в)
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            if (counter > 0)
+            {
+                counter--;
+                lblNumber.Text = undoNumbers[counter].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Это и есть нулевой ход.");
+            }
+        }
+
+        private void UndoNumberPlus()
+        {
+            if(isStart)
+            undoNumbers[counter] = int.Parse(lblNumber.Text);
+        }
+        #endregion
+    }
+}
