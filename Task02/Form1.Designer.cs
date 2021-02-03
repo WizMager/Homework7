e21d13dd4a8fc6aad875f4a963beffb8e068942c
Anton <wizmager@gmail.com>
2021-02-03 14:32:30 +0300

    Task02 completed

--- Task02/Form1.Designer.cs
+++ Task02/Form1.Designer.cs
@@ -29,13 +29,101 @@ namespace Task02
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblHint = new System.Windows.Forms.Label();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.playToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Location = new System.Drawing.Point(87, 87);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(144, 20);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCheck.Location = new System.Drawing.Point(87, 113);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(144, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblHint
+            // 
+            this.lblHint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHint.AutoSize = true;
+            this.lblHint.Location = new System.Drawing.Point(84, 45);
+            this.lblHint.Name = "lblHint";
+            this.lblHint.Size = new System.Drawing.Size(147, 13);
+            this.lblHint.TabIndex = 2;
+            this.lblHint.Text = "Нажмите Play чтобы начать";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playToolStrip});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(315, 24);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // playToolStrip
+            // 
+            this.playToolStrip.Name = "playToolStrip";
+            this.playToolStrip.Size = new System.Drawing.Size(41, 20);
+            this.playToolStrip.Text = "Play";
+            this.playToolStrip.Click += new System.EventHandler(this.playToolStrip_Click);
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.Location = new System.Drawing.Point(87, 68);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(43, 13);
+            this.lblCounter.TabIndex = 4;
+            this.lblCounter.Text = "counter";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(315, 198);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.lblHint);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximumSize = new System.Drawing.Size(360, 300);
+            this.MinimumSize = new System.Drawing.Size(250, 200);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblHint;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem playToolStrip;
+        private System.Windows.Forms.Label lblCounter;
     }
 }
 
--- Task02/Form1.cs
+++ Task02/Form1.cs
@@ -10,11 +10,80 @@ using System.Windows.Forms;
 
 namespace Task02
 {
+    //  2. Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, 
+    //     а человек пытается его угадать за минимальное число попыток.
+    //     Для ввода данных от человека используется элемент TextBox.
     public partial class Form1 : Form
     {
+        public int RandNumber { get; set; }
+        public int Counter { get; set; }
+
+        private event EventHandler NewGame;
+        private event EventHandler Check;
         public Form1()
         {
             InitializeComponent();
+            inputTextBox.Visible = false;
+            btnCheck.Visible = false;
+            lblCounter.Text = "";
+            NewGame += Form1_NewGame;
+            Check += Form1_Check;
+        }
+
+        private void Form1_Check(object sender, EventArgs e)
+        {
+            if (int.TryParse(inputTextBox.Text, out int number))
+            {
+                if(number == RandNumber)
+                {
+                    Counter++;
+                    lblHint.Text = $"Вы угадали! Загаданное число: {RandNumber}.";
+                    lblCounter.Text = $"Сделанные попытки: {Counter}";
+                    btnCheck.Visible = false;
+                    inputTextBox.Visible = false;
+                }
+                else if (number < RandNumber)
+                {
+                    Counter++;
+                    lblCounter.Text = $"Сделанные попытки: {Counter}";
+                    lblHint.Text = $"Введённое число меньше загаданного.";
+                }
+                else
+                {
+                    Counter++;
+                    lblCounter.Text = $"Сделанные попытки: {Counter}";
+                    lblHint.Text = $"Введённое число больше загаданного.";
+                }
+            }
+            else
+            {
+                lblHint.Text = "Вы ввели некоректное значение.";
+            }
+        }
+
+        private void Form1_NewGame(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            RandNumber = random.Next(0, 100);
+            Counter = 0;
+            MessageBox.Show(@"Загадано целое число от 0 до 100.");
+            lblHint.Text = @"Введите число и нажмите 'Check'.";
+            lblCounter.Text = $"Сделанные попытки: {Counter}";
+            inputTextBox.Visible = true;
+            inputTextBox.Text = "";
+            btnCheck.Visible = true;
+        }
+
+        private void playToolStrip_Click(object sender, EventArgs e)
+        {
+            //if (NewGame != null){NewGame.Invoke(sender, e);}
+            NewGame?.Invoke(sender, e);
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            //if (Check != null){Check.Invoke(sender, e);}
+            Check?.Invoke(sender, e);
         }
     }
 }
